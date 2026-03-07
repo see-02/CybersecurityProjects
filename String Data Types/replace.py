e85a2655ff11688db01e5727d13ee7dbb54a5c96
see-02 <englandsarah04@gmail.com>
2026-03-07 08:55:31 -0500

    Update replace.py

--- String Data Types/replace.py
+++ String Data Types/replace.py
@@ -1,16 +1,3 @@
-#Task 2:
-# Follow these steps:
-#   ● Create a new Python file in this folder called replace.py.
-#   ● Save the sentence: “The!quick!brown!fox!jumps!over!the!lazy!dog.” as a
-#   single string.
-#   ○ Reprint this sentence as “The quick brown fox jumps over the lazy
-#   dog.” using the replace() function to replace every exclamation
-#   mark “!” with a blank space.
-#   ○ Reprint that sentence as: “THE QUICK BROWN FOX JUMPS OVER
-#   THE LAZY DOG.” using the upper() function.
-#   ○ Reprint the sentence in reverse.
-#*****************************************************************************
-
 sentence = "The!quick!brown!fox!jumps!over!the!lazy!dog."
 
 # Process sentence without !
