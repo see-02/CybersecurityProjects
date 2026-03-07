ad42b63a7e9345ed0a884029dbfa5e05ea990221
see-02 <englandsarah04@gmail.com>
2026-03-07 08:42:03 -0500

    Create strings.py

---
+++ String Data Types/strings.py
@@ -0,0 +1,11 @@
+#Task 1:
+# Follow these steps:
+#   ● Create a new Python file in this folder called strings.py
+#   ● Declare a variable called hero that contains the value “$$$Superman$$$”
+#   ● Use the string-manipulation method strip() and print hero so that the
+#   output is: Superman
+
+hero = "$$$Superman$$$"
+
+# Strip away the $ from string
+print(hero.strip('$'))
