7552c3d23aa8124e3f9c86088c8f91f93846e10f
see-02 <englandsarah04@gmail.com>
2026-03-07 08:39:22 -0500

    Update num_challenge.py

--- Numerical Data Types/num_challenge.py
+++ Numerical Data Types/num_challenge.py
@@ -1,3 +1,18 @@
+#Challenge:
+# Use this opportunity to extend yourself by completing an optional challenge
+# activity.
+# Follow these steps:
+#   ● Create a new file called challenge.py.
+#   ● Write Python code to take the name of a user's favourite restaurant and
+#   store it in a variable called string_fav.
+#   ● Below this, write a statement to take in the user's favourite number. Use
+#   casting to store it in an integer variable called int_fav.
+#   ● Print out both of these using two separate print statements below what
+#   you have written. Be careful!
+#   ● Once this is working, try to cast string_fav to an integer. What happens?
+#   Add a comment in your code to explain why this is.
+
+
 #ask user's favorite restaurant and store it
 string_fav = input("What is the name of your favorite restaurant? ")
 
