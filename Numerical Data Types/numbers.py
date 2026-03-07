e2768e3b6c20bc75e184bf1b535033af4fc51159
see-02 <englandsarah04@gmail.com>
2026-03-07 08:26:40 -0500

    Create numbers.py

---
+++ Numerical Data Types/numbers.py
@@ -0,0 +1,20 @@
+#ask user for 3 different integers
+first_int = int(input("Please enter an integer: "))
+second_int = int(input("Please enter a different integer: "))
+third_int = int(input("Please enter a different integer: "))
+
+#print sum of all ints user gave
+sum = str(first_int + second_int + third_int)
+print("Total sum = " + sum)
+
+#print the first integer minus the second integer
+minus = str(first_int - second_int)
+print("First integer minus the second integer: " + minus)
+
+#print the third integer multiplied by the first integer
+product = str(third_int * first_int)
+print("Third integer multiplied by the first integer: " + product)
+
+#print the sum of all three integers divided by the third integer
+quotient = (first_int + second_int + third_int) / third_int
+print("The total sum divided by the third integer: " + str(quotient))
